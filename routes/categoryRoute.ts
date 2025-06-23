65a4f9f5de3c2eb13d915510bee62ddadc659d71
Rajan Sharma <email.rajan001@gmail.com>
2025-06-23 12:17:59 +0300

    SecondPush

--- routes/categoryRoute.ts
+++ routes/categoryRoute.ts
@@ -1,5 +1,6 @@
 import express from "express";
+import { createCategoryCotroller } from "../controller/categoryController/createCategoryController.ts";
 
 export const categoryRoute = express.Router();
 
-categoryRoute.post("/create-category", () => {});
+categoryRoute.post("/create-category", createCategoryCotroller);
