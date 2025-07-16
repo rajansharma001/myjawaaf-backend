11a15c513e652301e4ea3bcb3963fa4988cc525a
Rajan Sharma <email.rajan001@gmail.com>
2025-07-16 23:03:08 +0300

    types fixes

--- @types/express/index.d.ts
+++ @types/express/index.d.ts
@@ -3,7 +3,7 @@ import { User } from "../../model/userSchema"; // adjust path if needed
 declare global {
   namespace Express {
     interface Request {
-      user?: User;
+      user?: User | any;
     }
   }
 }
